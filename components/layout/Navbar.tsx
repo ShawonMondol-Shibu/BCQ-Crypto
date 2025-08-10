2193a7e0918e8cec2f7e88a8c4fd585407a91fd0
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-08-10 17:11:52 +0600

    navbar is updated

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 import navType from "@/types/type";
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 export default function Navbar() {
   const navLink: navType[] = [
@@ -9,18 +10,25 @@ export default function Navbar() {
     { name: "Explorer", url: "#" },
   ];
   return (
-    <nav>
-      <span className="">
-        <span className="text-(--primary)">BCQ</span>
-        Crypto
-      </span>
-      <ul>
-        {navLink.map((nav: navType, i: number) => (
-          <li key={i}>
-            <Link href={nav.url}>{nav.name}</Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="container m-auto px-2 py-2 flex items-center justify-between gap-5">
+      <div className="flex items-center gap-10">
+        <span className="text-2xl font-semibold">
+          <span className="text-(--primary)">BCQ</span>
+          Crypto
+        </span>
+        <ul className="flex items-center gap-5">
+          {navLink.map((nav: navType, i: number) => (
+            <li key={i}>
+              <Link href={nav.url}>{nav.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="space-x-5">
+        <Button>Login</Button>
+        <Button>Sign Up</Button>
+      </div>
     </nav>
   );
 }
